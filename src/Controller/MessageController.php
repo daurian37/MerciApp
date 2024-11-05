27882f01c57a4f74a4b480202f1b04d21eda40b9
daurian37 <dv.balenvokolo@gmail.com>
2024-11-05 18:05:50 +0100

    feat(testTechnique): Mise en place fonctionnalite de remerciement

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Repository\MessageRepository;
+use App\Entity\Message;
+use App\Form\MessageType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 
 class MessageController extends AbstractController
 {
@@ -17,6 +20,26 @@ class MessageController extends AbstractController
         
         return $this->render('message/index.html.twig', [
             'messages' => $messages,
+        ]);
+    }
+    
+    #[Route('/message/create', name: 'create_message')]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $message = new Message();
+        $form = $this->createForm(MessageType::class, $message);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $message->setCreatedAt(new \DateTime());
+            $em->persist($message);
+            $em->flush();
+
+            return $this->redirectToRoute('app_message');
+        }
+
+        return $this->render('message/create.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
