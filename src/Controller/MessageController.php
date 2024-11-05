522eee1c47b882549b4d8f73f44b8a0cb759d306
daurian37 <dv.balenvokolo@gmail.com>
2024-11-05 17:03:58 +0100

    feat(testTechnique): creation messageController

---
+++ src/Controller/MessageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MessageRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class MessageController extends AbstractController
+{
+    #[Route('/message', name: 'app_message')]
+    public function index(MessageRepository $messageRepository): Response
+    {
+        $messages = $messageRepository->findAll();
+        
+        return $this->render('message/index.html.twig', [
+            'messages' => $messages,
+        ]);
+    }
+}
